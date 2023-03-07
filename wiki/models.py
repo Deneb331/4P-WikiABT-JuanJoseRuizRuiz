f590a8ec1d74e7a10ce513156a65c4413a9b99fb
Deneb331 <juanjo1.seriousmail@gmail.com>
2023-03-07 19:13:22 +0000

    Add category model, basic index view and basic html base page

--- wiki/models.py
+++ wiki/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    description = models.CharField(max_length=1000)
+    slug = models.SlugField(max_length=200, unique=True)
+    feature_image = CloudinaryField('image', default='placeholder')
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name="post_like", blank=True)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
--- wiki/views.py
+++ wiki/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
-# Create your views here.
+from .models import Category, Post
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+class CategoryList(generic.ListView):
+    """
+    In this page we show a list with all the categories contained in the wiki.
+    """
+    model = Category
+    queryset = Category.objects.all()
+    template_name = "category_list.html"
